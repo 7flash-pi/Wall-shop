0ceee73c25b1cbe5d434ab88658f3616b52be77b
7flash-pi <82704654+7flash-pi@users.noreply.github.com>
2022-12-20 14:54:15 +0530

    category work

---
+++ src/components/Categories.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../Styles/categories.css'
+
+const Categories = () => {
+  return (
+    <div className='category'>
+        <label>category</label>
+        <div className="categoryGrid">
+            
+        </div>
+      
+    </div>
+  )
+}
+
+export default Categories
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
+import Categories from '../components/Categories';
 
 const Homepage = () => {
   return (
     <div className='Homepage'>
       <Navbar />
+      <Categories />
     </div>
   )
 }
