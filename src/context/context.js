8e788eaa6c35b66cee9da05cc0d6be046a1bc708
Ashish Raj <82704654+7flash-pi@users.noreply.github.com>
2022-12-29 06:14:01 -0800

    context

--- src/context/context.js
+++ src/context/context.js
@@ -1,1 +1,13 @@
 import { useContext } from 'react';
+
+import React from 'react';
+
+const AppContext=React.createContext();
+
+const AppContext.Provider = () => {
+  return (
+    <div>AppProvider</div>
+  )
+}
+
+export default AppProvider
