3d9894441f11fba095f9a187a9eaa0e7b9b3e91f
Ashish Raj <82704654+7flash-pi@users.noreply.github.com>
2023-01-01 17:18:25 +0530

    better category loading

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../Styles/categories.css';
 import SingleCategory from './SingleCategory';
+import anime from '../context/data'
+
 
 
 const Categories = () => {
+
+  const [catArr,SetCatArr]=useState([]);
+
+  useEffect(() => {
+    SetCatArr(anime);
+  },[])
   return (
     <div className='category'>
         <label>category</label>
         <div className="categoryGrid">
-          <SingleCategory name="anime" />
+          <SingleCategory name="anime" catArr={catArr} />
           <SingleCategory name="Nature"/>
           <SingleCategory name="LandScape"/>
   
--- src/components/SingleCategory.js
+++ src/components/SingleCategory.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import '../Styles/singleCategory.css';
-import anime from '../context/data';
 
-const SingleCategory = ( {name} ) => {
+
+const SingleCategory = ( {name,catArr} ) => {
   return (
     <div className='singleCategory'>
         <p>{name}</p>
         <div className="imgContainer">
          {
-          anime.map( img => 
+          catArr .map( img => 
               <img src={img.imgPath} key={img.id} alt={name}/>
             )
          }
