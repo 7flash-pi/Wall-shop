4e1f5284dfc513a93573ae81c9649d9e573d4354
Ashish Raj <82704654+7flash-pi@users.noreply.github.com>
2023-01-06 18:43:53 +0530

    footer structure

---
+++ src/components/PageFooter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../Styles/footer.css'
+
+const PageFooter = () => {
+  return (
+    <div className='pageFooter'>
+      PageFooter
+    </div>
+  )
+}
+
+export default PageFooter
